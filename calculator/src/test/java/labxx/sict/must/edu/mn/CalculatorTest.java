8bab1a524c0efed04e716f51646de568ce06f2a5
Timekilla-tech <ffjzff@gmail.com>
2025-05-09 14:09:08 +0800

    add @Test to testSubstract

--- calculator/src/test/java/labxx/sict/must/edu/mn/CalculatorTest.java
+++ calculator/src/test/java/labxx/sict/must/edu/mn/CalculatorTest.java
@@ -16,6 +16,7 @@ public class CalculatorTest {
     void testAdd() {
         assertEquals(5, calc.add(2, 3));
     }
+    @Test
     void testSubtract() {
         assertEquals(1, calc.subtract(3, 2));
     }
---
+++ calculator/src/test/java/labxx/sict/must/edu/mn/LongMultiTest.java
@@ -0,0 +1,10 @@
+package labxx.sict.must.edu.mn;
+
+import org.junit.jupiter.api.Test;
+
+public class LongMultiTest {
+    @Test
+    void testMultiply() {
+
+    }
+}
